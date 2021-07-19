ecce803b137f8008c74712114746f5f96a276f6c
Caio Felipe <devcaiofelipe15@gmail.com>
2021-07-19 08:07:30 -0300

    feat(despesas-receitas): salvar despesas e receitas no firebase

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,11 +1,75 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { SafeAreaView, Keyboard, TouchableWithoutFeedback, Alert } from 'react-native';
+import firebase from '../../services/firebaseConnection.js';
+import { format } from 'date-fns';
+import { useNavigation } from '@react-navigation/native'
+import Header from '../../components/Header/index.js';
+import { Background, Input, SubmitButton, SubmitText } from './styles';
+import Picker from '../../components/Picker';
+import { AuthContext } from '../../contexts/auth.js'
 
 
 export default function New() {
+    const [valor, setValor] = useState('');
+    const [tipo, setTipo] = useState('receita');
+    const navigation = useNavigation();
+    const { user: usuario } = useContext(AuthContext);
+
+
+    function handleSubmit() {
+        Keyboard.dismiss();
+        if(isNaN(parseFloat(valor)) || tipo === null) {
+            alert('Preencha todos os campos');
+            return
+        };
+        Alert.alert('Confirmando dados',
+                    `Tipo ${tipo} - Valor: ${valor}`,
+                    [
+                        { text: 'Cancelar', style: 'cancel' },
+                        { text: 'Continuar', onPress: () => handleAdd() }
+                    ])
+    };
+
+    async function handleAdd() {
+        let uid = usuario.uid;
+        let key = await firebase.database().ref('historico').child(uid).push().key;
+        await firebase.database().ref('historico').child(uid).child(key).set({
+            tipo: tipo,
+            valor: parseFloat(valor),
+            date: format(new Date(), 'dd/MM/yy')
+        });
+
+        let user = firebase.database().ref('users').child(uid);
+        await user.once('value').then((snapshot) => {
+            let saldo = parseFloat(snapshot.val().saldo);
+            tipo === 'despesa' ? saldo -= parseFloat(valor) : saldo += parseFloat(valor)
+            user.child('saldo').set(saldo);
+        });
+        Keyboard.dismiss()
+        setValor('');
+        navigation.navigate('Home');
+        
+    };
+
     return(
-        <View>
-            <Text>NEW</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss() }>
+            <Background>
+                <Header/>
+                <SafeAreaView style={{ alignItems: 'center' }}>
+                    <Input
+                    placeholder="Valor desejado"
+                    keyboardType="numeric"
+                    returnKeyType="next"
+                    onSubmitEditing={() => Keyboard.dismiss()}
+                    value={valor}
+                    onChangeText={(text) => setValor(text)}></Input>
+
+                    <Picker onChange={setTipo} tipo={tipo}/>
+                    <SubmitButton onPress={handleSubmit}>
+                        <SubmitText>Registrar</SubmitText>
+                    </SubmitButton>
+                </SafeAreaView>
+            </Background>
+        </TouchableWithoutFeedback>
     )
 };
