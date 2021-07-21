4cda55c93b23e454c3227afba5be604dc7a4fcfb
Caio Felipe <devcaiofelipe15@gmail.com>
2021-07-21 08:15:38 -0300

    feat(financeiro): adicionar feature de deletar uma despesa ou receita

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,8 @@ import React, { useContext, useState, useEffect } from 'react';
 import Header from '../../components/Header/index.js';
 import HistoricoList from '../../components/HistoricoList/index.js'
 import firebase from '../../services/firebaseConnection.js';
-import { format } from 'date-fns';
+import { Alert } from 'react-native';
+import { format, isBefore } from 'date-fns';
 import { AuthContext } from '../../contexts/auth';
 import { Background, Container, Nome, Saldo, Title, List } from './styles.js';
 
@@ -21,7 +22,7 @@ export default function Home() {
             });
             await firebase.database().ref('historico')
                                      .child(uid)
-                                     .orderByChild('date').equalTo(format(new Date(), 'dd/MM/yy'))
+                                     .orderByChild('date').equalTo(format(new Date(), 'dd/MM/yyyy'))
                                      .limitToLast(10)
                                      .on('value', (snapshot) => {
                                          setHistorico([]);
@@ -30,6 +31,7 @@ export default function Home() {
                                                  key: childItem.key,
                                                  tipo: childItem.val().tipo,
                                                  valor: childItem.val().valor,
+                                                 date: childItem.val().date,
                                              };
                                              setHistorico(oldArray => [...oldArray, list].reverse())
                                          })
@@ -37,6 +39,38 @@ export default function Home() {
                                 }
         loadList();
     }, []);
+
+    function handleDelete(data) {
+        const [day, month, year] = data.date.split('/');
+        const newDate = new Date(`${year}/${month}/${day}`);
+        const dateToday = format(new Date(), 'dd/MM/yyyy');
+        const [ dayToday, monthToday, yearToday ] = dateToday.split('/');
+        const newDateToday = new Date(`${yearToday}/${monthToday}/${dayToday}`)
+        console.log(newDateToday);
+        if(isBefore(newDate, newDateToday)) {
+            alert('Você não pode excluir um registro antigo');
+            return;
+        };
+        Alert.alert('Atenção', `Você deseja excluir ${data.tipo} - Valor: ${data.valor}`,
+        [
+            { text: 'Cancelar', style: 'cancel' },
+            { text: 'Confirmar', onPress: () => handleDeleteSuccess(data) },
+        ])
+    };
+
+    async function handleDeleteSuccess(data) {
+        await firebase.database().ref('historico')
+        .child(uid).child(data.key).remove()
+        .then(async() => {
+            let saldoAtual = saldo;
+            data.tipo === 'despesa' ? saldoAtual += parseFloat(data.valor) : saldoAtual -= parseFloat(data.valor)
+            await firebase.database().ref('users').child(uid).child('saldo').set(saldoAtual);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    };
+
     return(
         <Background>
             <Header/>
@@ -49,7 +83,7 @@ export default function Home() {
             showsVerticalScrollIndicator={false}
             data={historico}
             keyExtractor={item => item.key}
-            renderItem={({ item }) => <HistoricoList data={item}/> }/>
+            renderItem={({ item }) => <HistoricoList data={item} deleteItem={handleDelete}/> }/>
         </Background>
     
     )
